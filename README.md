d3374c6a8a5f52db4eea82abc8e3903704cb841e
Alexander Korolev <kilork@yandex.ru>
2019-08-20 20:12:56 +0200

    cargo generate rust library template

---
+++ README.md
@@ -0,0 +1,9 @@
+# {{project-name}} description
+
+## Legal
+
+Dual-licensed under `MIT` or the [UNLICENSE](http://unlicense.org/).
+
+## Features
+
+## Usage
---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+/*!
+# {{project-name}} description
+
+## Features
+
+## Usage
+*/
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
