3cc058ff52294bd44fc87f7207922f3ebb6224fa
Alexander Korolev <alexander.korolev.germany@gmail.com>
2025-05-28 09:41:37 +0200

    use lints and import README.md as docs for lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,15 @@
-/*!
-# {{project-name}} description
-
-## Features
-
-## Usage
-
-Add dependency to Cargo.toml:
-
-```toml
-[dependencies]
-{{project-name}} = "0.1"
-```
-
-*/
+#![doc = include_str!("../README.md")]
+#![warn(
+    missing_docs,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications,
+    variant_size_differences
+)]
 
 #[cfg(test)]
 mod tests {
